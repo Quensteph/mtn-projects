acec760aedd4b7b7a06b9c7d441f408f11cf69d4
Sergey Shcherbakov <yuramonogatari@gmail.com>
2021-06-21 11:05:53 +0000

    Update Project.java

--- module3/maven-task/src/main/java/com/test/Project.java
+++ module3/maven-task/src/main/java/com/test/Project.java
@@ -10,7 +10,12 @@ class Project {
   }
   
   public static void main(String[] args) {
-    System.out.println("Hellow MTN");
+    System.out.println("Hello MTN");
+    System.out.println("New line");
+    int a = 1;
+    int b = 2;
+    int c = a + b;
+    System.out.println(c);
   }
 
 }
